6f6adf50676760248ab174c12b4b74fd2b486526
sajjadfatehi <sajad.fatehi@snapp.cab>
2024-08-30 12:23:22 +0330

    remove useless line

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,41 +13,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
 
-//        google {
-//            content {
-//                includeGroupByRegex("com\\.android.*")
-//                includeGroupByRegex("com\\.google.*")
-//                includeGroupByRegex("androidx.*")
-//            }
-//        }
-//        mavenCentral()
-//        gradlePluginPortal()
-//
-//        maven {
-//            // google()
-//            url = uri("https://repo.snapp.tech/repository/android-repo-google/")
-//        }
-//        maven {
-//            // url "https://maven.google.com/"
-//            url= uri("https://repo.snapp.tech/repository/android-repo-maven-google/")
-//        }
-//
-//        maven {
-//            // mavenCentral()
-//            url =uri("https://repo.snapp.tech/repository/android-repo-maven/")
-//        }
-//        maven {
-//            // url "https://plugins.gradle.org/m2/"
-//            url=uri( "https://repo.snapp.tech/repository/android-repo-plugins/")
-//        }
-//        maven {
-//            // jcenter()
-//            url=uri( "https://repo.snapp.tech/repository/android-repo-jcenter-bintray/")
-//        }
-//
-//        // support HMS in Snapp LocationKit Module
-//        maven { url = uri("https://repo.snapp.tech/repository/android-repo-huawei/") }
-//
     }
 }
 
